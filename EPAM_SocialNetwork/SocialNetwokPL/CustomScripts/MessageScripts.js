c31f540ab6ab947243194cd510a9f0e0e7005c34
p0lich <a.polikarpov2014@gmail.com>
2021-07-20 12:53:46 +0400

    Styles updates. Bd dump added

---
+++ EPAM_SocialNetwork/SocialNetwokPL/CustomScripts/MessageScripts.js
@@ -0,0 +1,26 @@
+﻿function changeMessage(messageId, messageText) {
+    const sendBtn = document.getElementById("sendMessageId");
+
+    if (sendBtn.value === "Send") {
+        sendBtn.value = "Edit";
+        document.getElementById("messageInputId").value = messageText;
+        document.getElementById("editMessageId").value = messageId;
+    } else {
+        sendBtn.value = "Send";
+        document.getElementById("editMessageId").value = "";
+    }
+}
+
+$(function () {
+    $("form[name='messageSendForm']").validate({
+        rules: {
+            messageInput: {
+                required: true
+            }
+        },
+
+        messages: {
+            required: "You can't send empty messages"
+        },
+    });
+});
---
+++ EPAM_SocialNetwork/SocialNetwokPL/CustomScripts/PageRedirections.js
@@ -0,0 +1,19 @@
+﻿$('#toRegisterPage').click(function () {
+    location.href = '/Pages/Registration/Register.cshtml';
+})
+
+$('#toLoginPage').click(function () {
+    location.href = '/Pages/Login.cshtml';
+})
+
+$('#toProfilePage').click(function () {
+    location.href = '/Pages/UserPersonalPage.cshtml';
+})
+
+$('#toSearchPage').click(function () {
+    location.href = '/Pages/UserSearch.cshtml';
+})
+
+$('#signOut').click(function () {
+    location.href = '/Pages/LogOut.cshtml';
+})
